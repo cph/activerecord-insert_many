07849b957b01f52b49ad273ecf74c0d8544016d1
Bob Lail <bob.lail@cph.org>
2015-09-07 18:21:51 -0500

    [refactor] Extracted activerecord/insert_many from idioms (4m)

--- lib/activerecord/insert_many.rb
+++ lib/activerecord/insert_many.rb
@@ -1,7 +1,31 @@
 require "activerecord/insert_many/version"
+require "active_record"
+require "active_record/connection_adapters/postgresql_adapter"
 
-module Activerecord
-  module InsertMany
-    # Your code goes here...
+module InsertMany
+  def insert_many(fixtures)
+    connection.insert_many(fixtures, table_name)
   end
 end
+
+module InsertManyStatement
+  def insert_many(fixtures, table_name=self.table_name)
+    return if fixtures.empty?
+
+    columns = schema_cache.columns_hash(table_name)
+
+    sample = fixtures.first
+    key_list = sample.map { |name, value| quote_column_name(name) }
+
+    value_lists = fixtures.map do |fixture|
+      fixture.map do |name, value|
+        quote(value, columns[name])
+      end
+    end
+
+    execute "INSERT INTO #{quote_table_name(table_name)} (#{key_list.join(', ')}) VALUES #{value_lists.map { |value| "(#{value.join(', ')})" }.join(",")}", "Fixture Insert"
+  end
+end
+
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send :include, InsertManyStatement
+ActiveRecord::Base.extend InsertMany
